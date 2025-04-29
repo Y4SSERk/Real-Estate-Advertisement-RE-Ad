b06be07e0a077cbb7a4e55d60dbf8ac7d16d98eb
YASSER <yasser.khattach@gmail.com>
2025-04-30 00:02:45 +0100

    connect to PostgreSQL

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -81,14 +81,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'REAdDB',       # Database name
+        'USER': 'admin',         # PostgreSQL username
+        'PASSWORD': 'admin123', # PostgreSQL password
+        #'HOST': 'localhost',         Use 'db' if using Docker
+        #'PORT': '5432',              Default PostgreSQL port
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
