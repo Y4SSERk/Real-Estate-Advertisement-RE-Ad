03660bbb36b230acb1416631efb88794cc1411f5
YASSER <yasser.khattach@gmail.com>
2025-05-11 22:23:13 +0100

    fix errors

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path  
-from .views import hello_world  
+from .views import hello_world, get_properties, create_property
 
 urlpatterns = [  
     path('hello/', hello_world),  
-]  
+    path('properties/', get_properties),
+    path('properties/create/', create_property, name='create_property'),
+]
--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -1,6 +1,24 @@
 from rest_framework.decorators import api_view  
 from rest_framework.response import Response  
+from rest_framework import status
+from .models import Property, PropertyImage
+from apps.users.models import User
+from .serializer import PropertySerializer, PropertyImageSerializer, UserSerializer
 
 @api_view(['GET'])  
 def hello_world(request):  
     return Response({"message": "Django API is working!"})  
+
+@api_view(['GET'])
+def get_properties(request):  
+    properties = Property.objects.all()  
+    serializer = PropertySerializer(properties, many=True)  
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_property(request):  
+    serializer = PropertySerializer(data=request.data)  
+    if serializer.is_valid():  
+        serializer.save()  
+        return Response(serializer.data, status=status.HTTP_201_CREATED)  
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
